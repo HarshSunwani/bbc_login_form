61cc2fb2ca4f2bfdd4e310e662711cc59b6509b8
HarshSunwani <harshsunwani11@gmail.com>
2023-02-08 20:58:59 +0530

    basic setup

---
+++ stories/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./loginForm.css";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+interface LoginFormProps {
+  handlechange: () => void;
+  handleLogin: () => void;
+}
+
+export const LoginForm = ({ }: LoginFormProps) => (
+    <form action=""></form>
+)
